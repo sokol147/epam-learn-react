a3ed0f7a74b5ed6c42d794539080b67d2e47d5e1
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2020-09-23 23:36:51 +0300

    HW-5

--- src/js/App.js
+++ src/js/App.js
@@ -1,126 +1,127 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import Modal from "./components/Modal/Modal";
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.hideModal = this.hideModal.bind(this);
-        this.openModal = this.openModal.bind(this);
-        this.state = {
-            isModalOpen: false,
-        };
+const App = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-        this.movieList = [
-            {
-                id: "1",
-                title: "film 1",
-                releaseDate: "2020-07-22",
-                url: "kinopoisk/action/1",
-                year: "2010",
-                genre: "action",
-                overview: "",
-                runTime: "127",
-                poster: "../src/img/poster-1.jpg",
-            },
-            {
-                id: "2",
-                title: "film 2",
-                releaseDate: "2020-07-22",
-                url: "kinopoisk/action/1",
-                year: "2010",
-                genre: "action",
-                overview: "",
-                runTime: "127",
-                poster: "../src/img/poster-2.jpg",
-            },
-            {
-                id: "3",
-                title: "film 3",
-                releaseDate: "2020-07-22",
-                url: "kinopoisk/action/1",
-                year: "2010",
-                genre: "action",
-                overview: "",
-                runTime: "127",
-                poster: "../src/img/poster-3.jpg",
-            },
-            {
-                id: "4",
-                title: "film 4",
-                releaseDate: "2020-07-22",
-                url: "kinopoisk/action/1",
-                year: "2010",
-                genre: "action",
-                overview: "",
-                runTime: "127",
-                poster: "../src/img/poster-4.jpg",
-            },
-            {
-                id: "5",
-                title: "film 5",
-                releaseDate: "2020-07-22",
-                url: "kinopoisk/action/1",
-                year: "2010",
-                genre: "action",
-                overview: "",
-                runTime: "127",
-                poster: "../src/img/poster-5.jpg",
-            },
-            {
-                id: "6",
-                title: "film 6",
-                releaseDate: "2020-07-22",
-                url: "kinopoisk/action/1",
-                year: "2010",
-                genre: "action",
-                overview: "",
-                runTime: "127",
-                poster: "../src/img/poster-6.jpg",
-            },
-            {
-                id: "7",
-                title: "film 7",
-                releaseDate: "2020-07-22",
-                url: "kinopoisk/action/1",
-                year: "2010",
-                genre: "action",
-                overview: "",
-                runTime: "127",
-                poster: "../src/img/poster-7.jpg",
-            },
-        ];
+    const selectedMovie = useCallback(
+        () => {
+            randomMovie(movieList);
+        },
+        [movieList]
+    )
+
+    const movieList = [
+        {
+            id: "1",
+            title: "film 1",
+            releaseDate: "2020-07-22",
+            url: "kinopoisk/action/1",
+            year: "2010",
+            genre: "action",
+            rating: "4.5",
+            overview: "Long description",
+            runTime: "127",
+            poster: "../src/img/poster-1.jpg",
+        },
+        {
+            id: "2",
+            title: "film 2",
+            releaseDate: "2020-07-22",
+            url: "kinopoisk/action/1",
+            year: "2010",
+            genre: "action",
+            overview: "",
+            runTime: "127",
+            poster: "../src/img/poster-2.jpg",
+        },
+        {
+            id: "3",
+            title: "film 3",
+            releaseDate: "2020-07-22",
+            url: "kinopoisk/action/1",
+            year: "2010",
+            genre: "action",
+            overview: "",
+            runTime: "127",
+            poster: "../src/img/poster-3.jpg",
+        },
+        {
+            id: "4",
+            title: "film 4",
+            releaseDate: "2020-07-22",
+            url: "kinopoisk/action/1",
+            year: "2010",
+            genre: "action",
+            overview: "",
+            runTime: "127",
+            poster: "../src/img/poster-4.jpg",
+        },
+        {
+            id: "5",
+            title: "film 5",
+            releaseDate: "2020-07-22",
+            url: "kinopoisk/action/1",
+            year: "2010",
+            genre: "action",
+            overview: "",
+            runTime: "127",
+            poster: "../src/img/poster-5.jpg",
+        },
+        {
+            id: "6",
+            title: "film 6",
+            releaseDate: "2020-07-22",
+            url: "kinopoisk/action/1",
+            year: "2010",
+            genre: "action",
+            overview: "",
+            runTime: "127",
+            poster: "../src/img/poster-6.jpg",
+        },
+        {
+            id: "7",
+            title: "film 7",
+            releaseDate: "2020-07-22",
+            url: "kinopoisk/action/1",
+            year: "2010",
+            genre: "action",
+            overview: "",
+            runTime: "127",
+            poster: "../src/img/poster-7.jpg",
+        },
+    ];
+
+    function randomMovie(filmList) {
+        return filmList[Math.floor(Math.random()*items.length)];
     }
 
-    hideModal() {
-        this.setState({
-            isModalOpen: false,
-        });
+    function hideModal() {
+        setIsModalOpen(false);
     }
 
-    openModal() {
-        this.setState({
-            isModalOpen: true,
-        });
+    function openModal() {
+        setIsModalOpen(true);
     }
 
-    render() {
-        return (
-            <>
-                <Header handleModalOpen={this.openModal} />
-                <Main movieList={this.movieList} />
-                <Footer />
-                <Modal
-                    isModalOpen={this.state.isModalOpen}
-                    modalType="add"
-                    movie={{}}
-                    modalTitle="Add movie"
-                    handelModalClose={this.hideModal}
-                />
-            </>
-        );
-    }
+    return (
+        <>
+            <Header handleModalOpen={openModal} />
+            <Main movieList={movieList} />
+            <Footer />
+            <Modal
+                isModalOpen={isModalOpen}
+                modalType="add"
+                movie={{}}
+                modalTitle="Add movie"
+                handelModalClose={hideModal}
+            />
+        </>
+    );
 }
+
+export default App;
